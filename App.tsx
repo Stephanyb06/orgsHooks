17dcc2e8fa43c696348754887e5ab8d0f4a037d8
STEPHAN� BRAGA DOS SNTOS <stephany.3279@aluno.pr.senac.br>
2025-05-09 11:57:19 -0300

    chore: initial react native projeto setup

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function App() {
+  return <SafeAreaView>
+    <Text>Hello, world!</Text>
+  </SafeAreaView>
+}
+
+export default App;
